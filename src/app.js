870bfaab78f14ef4de48f1676bf21c1eb2482353
William <lopezwilliam177@gmail.com>
2021-06-29 14:14:34 -0300

    add code smell

--- src/app.js
+++ src/app.js
@@ -3,12 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.status(200).send("Hello World!");
+  try {
+    res.status(200).send("Hello World!");
+  } catch (error) {}
 });
-
-app.get("/", async (req, res) => {
-  res.status(200).send("Hello World!");
-});
-
 
 module.exports = app;
