22d08202cfd1228017b36b391f6dc49fefa04490
William <lopezwilliam177@gmail.com>
2021-06-29 15:37:57 -0300

    add code smell

--- src/app.js
+++ src/app.js
@@ -2,15 +2,13 @@ const express = require("express");
 
 const app = express();
 
+const op = require("./operacion")
+
+
+
 app.get("/", async (req, res) => {
-    const smell = "Hello World!"
-   try {
-    
-    res.status(200).send(smell);
-    return smell
-   } catch (error) {
-       return semell
-   }
+    const suma = op.Producto(2,3)
+    res.status(200).send("Hello World!");
 });
 
 module.exports = app;
---
+++ src/operacion.js
@@ -0,0 +1,9 @@
+function Producto (a, b) {
+const result = a * b;
+if (result == null){
+	return result;
+}
+return result
+}
+
+mudule.export = { Producto }
