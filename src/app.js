7d00566b48391da9c7186c3d26ffa7abb270fc59
William <lopezwilliam177@gmail.com>
2021-06-30 16:50:01 -0300

    add build

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const op = require("./operacion");
 
 app.get("/", async (req, res) => {
-  const producto = op.Producto(2, 3);
+  const producto = op.Producto(2, 2);
   res.status(200).send(`El Resultado del Producto es ${producto}`);
 });
 
